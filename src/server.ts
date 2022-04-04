7feb1883066f505c805efe713f381c38cf4fab6d
Tobi-TNMT <tontnm@gmail.com>
2022-04-04 16:15:18 +0700

    fix reviewer comment

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles, isUrl} from './util/util';
 
@@ -31,7 +31,7 @@ import {filterImageFromURL, deleteLocalFiles, isUrl} from './util/util';
 
   //! END @TODO1
 
-  app.get( "/filteredimage", async ( req, res ) => {
+  app.get( "/filteredimage", async ( req: Request, res: Response ) => {
     //query parameter from requirement
     let image_url:string = req.query.image_url;
 
@@ -64,7 +64,7 @@ import {filterImageFromURL, deleteLocalFiles, isUrl} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
